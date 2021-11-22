c202dea6d4f11b7660835c7e0e7ab58ea518d555
bayel04 <93245123+bayel04@users.noreply.github.com>
2021-11-22 12:23:25 +0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+ <head>
+  <body bgcolor=white background="https://png.pngtree.com/back_origin_pic/04/21/36/e79bec6267f3f0a959dbd8581e7201b6.jpg">
+  <meta charset="utf-8">
+  <title>Круглые изображения</title>
+  <style>
+   .parent {
+  padding: 10px;
+}
+img {
+  position: relative;
+  top: 80%; 
+  left: 80%;
+  height: 20%;
+  width: 20%;
+}
+   .round {
+    border-radius: 10000px; /* Радиус скругления */
+    border: px solid green; /* Параметры рамки */
+    box-shadow: 0 0 7px #666; /* Параметры тени */
+   
+  </style>
+ </head>
+ <body>
+
+
+   <p><img src="https://i.pinimg.com/736x/03/d7/e7/03d7e7c02f41dcbc04b0d84450587bff.jpg" alt="" width="220" height="200" class="round"></p>
+<p>Name.  :   Bayel    </p>
+<hr>
+<p>Surname:   Askaruulu     </p>
+<hr>
+<p>Age    :   16             </p>
+<hr>
+<p>Class  :.  Csc-20a.       </p>
+<hr>
+ <div class="container">
+      
+        <form class="modal-content animate" action="https://www.instagram.com/askarov__ba1el/">
+    <div class="imgcontainer">
+      <span onclick="document.getElementById('id01').style.display='none'" class="close" title="Close Modal">×</span>
+      <button type="submit">Instagram</button>
+  
+
+   <form class="modal-content animate" action="https://chat.whatsapp.com/BUxw2oEqWKEAbZEaRbk2yq">
+    <div class="imgcontainer">
+      <span onclick="document.getElementById('id02').style.display='none'" class="close" title="Close Modal">×</span>
+      <button type="submit">Wattsapp group</button>  
+
+ <form class="modal-content animate" action="http://alatoo.edu.kg/#gsc.tab=0">
+    <div class="imgcontainer">
+      <span onclick="document.getElementById('id03').style.display='none'" class="close" title="Close Modal">×</span>
+      <button type="submit">Ala-Too</button>      
+      </label>
+    </div>
+
+</body>
+</html>
